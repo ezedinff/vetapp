424e3bc0dedff23559360ece03dbe4ae9d3a82de
J@tin <ezedin.fedlu@gmail.com>
2021-07-15 15:52:29 +0300

    update dashboard

--- src/app/pages/Admin/Dashboard/index.tsx
+++ src/app/pages/Admin/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import EarningCard from 'app/components/dashboard/Board';
 import { Grid } from '@material-ui/core';
 import Sales from 'app/components/dashboard/Sales';
+import TopProducts from 'app/components/dashboard/Products';
 const Dashboard = () => {
   return (
     <Grid container spacing={2}>
@@ -20,6 +21,9 @@ const Dashboard = () => {
       <Grid item lg={3} md={6} sm={6} xs={12}>
         {' '}
         <EarningCard />
+      </Grid>
+      <Grid item lg={4} md={6} xl={3} xs={12}>
+        <TopProducts />
       </Grid>
       <Grid item lg={8} md={12} xl={9} xs={12}>
         <Sales />
