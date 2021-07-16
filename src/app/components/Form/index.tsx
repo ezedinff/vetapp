6516841d019f96dc9b227cee856351c0847d2651
J@tin <ezedin.fedlu@gmail.com>
2021-07-16 19:38:43 +0300

    submiting form fixed

--- src/app/components/Form/index.tsx
+++ src/app/components/Form/index.tsx
@@ -140,7 +140,8 @@ const Form: FC<Props> = ({
 }) => {
   const submit = value => {
     console.log(value);
-    alert(JSON.stringify(value));
+    submitForm(value);
+    // alert(JSON.stringify(value));
   };
   const {
     handleSubmit,
